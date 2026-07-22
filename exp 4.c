d10864a1f1ebd9353e54036e1492a5b9c4f4a359
SINDHIYA-12345 <192421429.simats@saveetha.com>
2026-07-22 11:36:19 +0530

    Create exp 4.c

---
+++ exp 4.c
@@ -0,0 +1,75 @@
+sindhiya(192421429)
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[200];
+    int i = 0;
+
+    printf("Enter the program:\n");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0')
+    {
+        // Ignore spaces, tabs and new lines
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+        {
+            i++;
+            continue;
+        }
+
+        // Ignore single-line comment
+        if (str[i] == '/' && str[i + 1] == '/')
+        {
+            break;
+        }
+
+        // Ignore multi-line comment
+        if (str[i] == '/' && str[i + 1] == '*')
+        {
+            i += 2;
+            while (str[i] != '\0' && !(str[i] == '*' && str[i + 1] == '/'))
+                i++;
+
+            if (str[i] != '\0')
+                i += 2;
+
+            continue;
+        }
+
+        // Identifier
+        if (isalpha(str[i]) || str[i] == '_')
+        {
+            printf("%c --> Identifier\n", str[i]);
+        }
+
+        // Constant
+        else if (isdigit(str[i]))
+        {
+            printf("%c --> Constant\n", str[i]);
+        }
+
+        // Operator
+        else if (str[i] == '+' || str[i] == '-' || str[i] == '*' ||
+                 str[i] == '/' || str[i] == '=' || str[i] == '<' ||
+                 str[i] == '>' || str[i] == '%')
+        {
+            printf("%c --> Operator\n", str[i]);
+        }
+
+        i++;
+    }
+
+    return 0;
+
+input:
+a = b + 20 
+output:
+a --> Identifier
+= --> Operator
+b --> Identifier
++ --> Operator
+2 --> Constant
+0 --> Constant
