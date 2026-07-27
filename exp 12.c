8264676535349d749a205a3940ef7d2956447521
SINDHIYA-12345 <192421429.simats@saveetha.com>
2026-07-27 10:57:46 +0530

    Create exp 12.c

---
+++ exp 12.c
@@ -0,0 +1,80 @@
+ sindhiya(192421429)
+
+   #include <stdio.h>
+#include <string.h>
+
+char input[20];
+int i = 0;
+
+void E();
+void EP();
+void T();
+void TP();
+void F();
+
+void E()
+{
+    T();
+    EP();
+}
+
+void EP()
+{
+    if (input[i] == '+')
+    {
+        i++;
+        T();
+        EP();
+    }
+}
+
+void T()
+{
+    F();
+    TP();
+}
+
+void TP()
+{
+    if (input[i] == '*')
+    {
+        i++;
+        F();
+        TP();
+    }
+}
+
+void F()
+{
+    if (input[i] == 'i')
+        i++;
+    else if (input[i] == '(')
+    {
+        i++;
+        E();
+        if (input[i] == ')')
+            i++;
+    }
+}
+
+int main()
+{
+    printf("Enter input: ");
+    scanf("%s", input);
+
+    E();
+
+    if (input[i] == '\0')
+        printf("String Accepted");
+    else
+        printf("String Rejected");
+
+    return 0;
+}
+
+input:
+i+i*i
+
+Output:
+Enter input: i+i*i
+String Accepted
